7819dd2d2d5bb6936c7cb804c50665f275e84610
Nicolás Arias <ndariass@unal.edu.co>
2016-04-09 16:30:36 -0500

    Displaying fake markers

---
+++ WebAPP/js/banks.js
@@ -0,0 +1,22 @@
+BANKS = [
+    {
+        id: 1,
+        name: 'Davivienda',
+        address: 'Kra. 1 # 1 - 1',
+        position: {lat: 4.6531691, lng: -74.0589639},
+    },
+
+    {
+        id: 2,
+        name: 'Popular',
+        address: 'Kra. 2 # 2 - 2',
+        position: {lat: 4.6515415, lng: -74.0574888},
+    },
+
+    {
+        id: 3,
+        name: 'Occidente',
+        address: 'Kra. 3 # 3 - 3',
+        position: {lat: 4.650706, lng: -74.0599931},
+    },
+];
--- WebAPP/js/map.js
+++ WebAPP/js/map.js
@@ -1,23 +1,45 @@
 var map;
+var markers;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 4.6447628, lng: -74.1086459},
-        zoom: 12
+        center: {lat: 4.6515167, lng: -74.0594862},
+        zoom: 16,
     });
 
-    var marker = new google.maps.Marker({
-        position: {lat: 4.6447628, lng: -74.1086359},
+    markers = new Array(BANKS.length);
+
+    for (var i = 0; i < BANKS.length; i++){
+        setMarker(BANKS[i], i);
+    }
+
+
+    for (var i = 0; i < BANKS.length; i++){
+        marker[i].addListener('click', function () {
+
+            for (var j = 0; j < BANKS.length; j++){
+                if (i != j)
+                    marker[j].close();
+            }
+        });
+    }
+}
+
+function setMarker(data, index){
+    console.log(data.position);
+    markers[index] = new google.maps.Marker({
+        position: data.position,
         map: map,
         title: 'Hello World!'
     });
 
-    var contentString = 'This is a message!';
+    var contentString = '<strong>' + data.name + '</strong> </br>';
+    var contentString = contentString + data.address;
 
     var infowindow = new google.maps.InfoWindow({
         content: contentString
     });
 
-    marker.addListener('click', function () {
-        infowindow.open(map, marker);
+    markers[index].addListener('click', function () {
+        infowindow.open(map, markers[index]);
     });
 }
