140a28ff5d7360702e94e5fcb4b93a8a3db4e158
Nicolás Arias <ndariass@unal.edu.co>
2016-04-09 20:34:09 -0500

    Adding pins of banks nearby

--- WebAPP/js/banks.js
+++ WebAPP/js/banks.js
@@ -1,22 +1,79 @@
 BANKS = [
     {
         id: 1,
-        name: 'Davivienda',
-        address: 'Kra. 1 # 1 - 1',
-        position: {lat: 4.6531691, lng: -74.0589639},
+        name: 'Banca mía principal',
+        address: 'Cra. 9 # 66-25',
+        position: {lat: 4.651465, lng: -74.060089},
     },
 
     {
         id: 2,
-        name: 'Popular',
-        address: 'Kra. 2 # 2 - 2',
-        position: {lat: 4.6515415, lng: -74.0574888},
+        name: 'Banco de Bogotá',
+        address: 'Cra. 11 # 68 - 09',
+        position: {lat: 4.653643, lng: -74.061134},
     },
 
     {
         id: 3,
-        name: 'Occidente',
-        address: 'Kra. 3 # 3 - 3',
-        position: {lat: 4.650706, lng: -74.0599931},
+        name: 'Banco de Bogotá',
+        address: 'Kra. 7 # 67-35',
+        position: {lat: 4.650760, lng: -74.058756},
     },
+
+    {
+        id: 4,
+        name: 'BBVA',
+        address: 'Cll. 67 # 7-35',
+        position: {lat: 4.650404, lng: -74.058694},
+    },
+
+    {
+        id: 5,
+        name: 'Banco de occidente',
+        address: 'Kra. 7 # 62-15',
+        position: {lat: 4.647035, lng: -74.060288},
+    },
+
+    {
+        id: 6,
+        name: 'Colpatria',
+        address: 'Cra. 7 # 70A-40',
+        position: {lat: 4.653708, lng: -74.055771},
+    },
+
+    {
+        id: 7,
+        name: 'Banco de Occidente',
+        address: 'Cra. 7 # 71-28',
+        position: {lat: 4.654145, lng: -74.055557},
+    },
+
+    {
+        id: 8,
+        name: 'Davivienda',
+        address: 'Cra. 7 # 71-65',
+        position: {lat: 4.654796, lng: -74.055955},
+    },
+
+    {
+        id: 9,
+        name: 'Banco GNB Sudameris',
+        address: 'Cll. 71 #11-02',
+        position: {lat: 4.656496, lng: -74.059482},
+    },
+
+    {
+        id: 10,
+        name: 'Banco Caja Social',
+        address: 'Cra. 7 # 10-57',
+        position: {lat: 4.656871, lng: -74.058585},
+    },
+
+    {
+        id: 11,
+        name: 'Banco Fiduciaria La Previsora SA',
+        address: 'Cll. 72 # 10-03',
+        position: {lat: 4.656466, lng: -74.058069},
+    },
+
 ];
