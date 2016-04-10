9de22366fdbe530e16cfae645946fddd1806a16f
walcher <walter887@gmail.com>
2016-04-10 14:42:05 -0500

    Video

--- WebAPP/index.php
+++ WebAPP/index.php
@@ -19,10 +19,12 @@
     <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo"></a>
       <ul class="right hide-on-med-and-down">
         <li><a href="index.php">Inicio</a></li>
+        <li><a href="video.html">Video</a></li>
       </ul>
 
       <ul id="nav-mobile" class="side-nav">
         <li><a href="index.php">Inicio</a></li>
+         <li><a href="video.html">Video</a></li>
       </ul>
       <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
     </div>
